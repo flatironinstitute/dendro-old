a5951d87b1e0f959c49aec5e01bea70e8b5b3f01
Jeremy Magland <jmagland@flatironinstitute.org>
2023-10-26 09:55:45 -0400

    update project id in test

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -3,8 +3,8 @@ from protocaas.client import load_project
 
 def test_load_project():
     # if this project or file disappears then we'll need to update that here
-    project_id = 'ed104ee8'
-    file_name = 'imported/000618/sub-paired-kampff/sub-paired-kampff_ses-paired-kampff-2014-11-25-Pair-3-0_ecephys.nwb'
+    project_id = 'ae9596fb'
+    file_name = 'imported/000618/sub-paired-english/sub-paired-english_ses-paired-english-m108-191125-163508_ecephys.nwb'
     project = load_project(project_id)
     file = project.get_file(file_name)
     assert file.get_url().startswith('https://')
