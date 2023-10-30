45647067d1225292249723fd18d64fa65f11e9c5
Jeremy Magland <jmagland@flatironinstitute.org>
2023-10-30 14:04:44 -0400

    incr python version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 setup(
     name='dendro',
